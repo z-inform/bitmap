d75140234adc001ae3e3c93bba6da26d66ae5ce0
Zakhar <z.inform@mail.ru>
2021-01-28 03:10:19 +0300

    First successful draw. Not truncating trailing bits

---
+++ draw.cpp
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+#define SQR "\xe2\x96\xa0"
+
+const uint32_t separator = 0xEE77;
+
+int decode(uint8_t** ptr, size_t* size, size_t* width, FILE* desc);
+int draw(uint8_t* ptr, size_t size, size_t width);
+
+int main(){
+
+
+    FILE* fptr = fopen("hexData", "rb");
+    uint8_t* arr = nullptr;
+    size_t size = 0;
+    size_t width = 0;
+
+    decode(&arr, &size, &width, fptr);
+    printf("size: %d; width: %d\n", size, width);
+    printf("data: ");
+    for(int i = 0; i < size; i++){
+        printf("%x", arr[i]);
+    }
+    
+
+    draw(arr, size, 11);
+
+    fclose(fptr);
+    free(arr);
+}
+
+int draw(uint8_t* ptr, size_t size, size_t width){
+    
+    int byteCounter = 0;
+
+    printf("\n");
+
+    for(; byteCounter < size; byteCounter++){
+
+
+        for(int i = 1; i <= 8; i++){
+            if( (byteCounter * 8 + i - 1) % width == 0 ) printf("\n");
+            if( ((ptr[byteCounter] & (1 << (8 - i))) >> (8 - i)) == 0 ) printf("\033[107m  \e[0m");
+            else printf("\033[45m  \e[0m");
+        }
+
+
+    }
+
+
+
+    printf("\n");
+    return 0;
+}
+
+
+int decode(uint8_t** ptr, size_t* size, size_t* width, FILE* desc){
+    uint32_t sepCheck = 0;
+    size_t localSize = 0;
+
+    fread(&localSize, sizeof(size), 1, desc);
+    fread(&sepCheck, 2, 1, desc);
+
+    if( sepCheck != separator ){
+        printf("Error when parsing data size. Expected separator 0x%X, got 0x%X\n", sepCheck, separator);
+        return 1;
+    }
+
+    *size = localSize;
+
+    fread(width, sizeof(width), 1, desc);
+    fread(&sepCheck, 2, 1, desc);
+    if( sepCheck != separator ){
+        printf("Error when parsing width. Expected separator 0x%X, got 0x%X\n", sepCheck, separator);
+        return 1;
+    }
+    
+    *ptr = (uint8_t*) calloc(1, localSize);
+    fread(*ptr, localSize, 1, desc);
+
+    fread(&sepCheck, 2, 1, desc);
+    if( sepCheck != separator ){
+        printf("Error when parsing width. Expected separator 0x%X, got 0x%X\n", sepCheck, separator);
+        return 1;
+    }
+
+    return 0;
+}
--- filegen.cpp
+++ filegen.cpp
@@ -5,7 +5,7 @@
 
 const uint32_t separator = 0xEE77;
 
-int toHexFile(void* src, size_t dataSize, size_t width, FILE* dest);
+int toHexFile(void* src, size_t* dataSize, size_t* width, FILE* dest);
 int toBitmapFile(void* src, size_t dataSize, size_t width, FILE* dest);
 int printData(const void* ptr, size_t dataSize, FILE* dest);
 
@@ -15,54 +15,60 @@ int printData(const void* ptr, size_t dataSize, FILE* dest);
 int main(){
 
 
-    uint8_t* graph = (uint8_t*) calloc(17, 1);
-
-//fff00710c5191341682c898a308600fff  
+    uint8_t* graph = (uint8_t*) calloc(16, 1);
 
 /*
-11111111111
-11000000001
-10000100001
-10001010001
-10010001001
-10100000101
-10010001001
-10001010001
-10000100001
-10000000001
-11111111111
+
+11111111
+11111000
+00000110
+00010000
+11000101
+00011001
+00010011
+01000001
+01100100
+01001100
+01010001
+10000100
+00110000
+00000111
+11111111
+10000000
 
 
 
 
-
-
+121 bit
+128 cap
+16 byte
 
 
 */
 
-    graph[0] =  0xff;
-    graph[1] =  0xf0;
-    graph[2] =  0x07;
-    graph[3] =  0x10;
-    graph[4] =  0xc5;
-    graph[5] =  0x19;
-    graph[6] =  0x13;
-    graph[7] =  0x41;
-    graph[8] =  0x68;
-    graph[9] =  0x2c;
-    graph[10] = 0x89;
-    graph[11] = 0x8a;
-    graph[12] = 0x30;
-    graph[13] = 0x86;
-    graph[14] = 0x00;
-    graph[15] = 0xff;
-    graph[16] = 0xf0;
+    graph[0] =  0b11111111;
+    graph[1] =  0b11111000;
+    graph[2] =  0b00000110;
+    graph[3] =  0b00010000;
+    graph[4] =  0b11000101;
+    graph[5] =  0b00011001;
+    graph[6] =  0b00010011;
+    graph[7] =  0b01000001;
+    graph[8] =  0b01100100;
+    graph[9] =  0b01001100;
+    graph[10] = 0b01010001;
+    graph[11] = 0b10000100;
+    graph[12] = 0b00110000;
+    graph[13] = 0b00000111;
+    graph[14] = 0b11111111;
+    graph[15] = 0b10000000;
 
 
 
     FILE* fptr = fopen("hexData", "wb");
-    toHexFile(graph, 17, 11, fptr);
+    size_t size = 16;
+    size_t width = 11;
+    toHexFile(graph, &size, &width, fptr);
 
     fclose(fptr);
     free(graph);
@@ -70,12 +76,12 @@ int main(){
     return 0;
 }
 
-int toHexFile(void* src, size_t dataSize, size_t width, FILE* dest){
-    fwrite(&dataSize, sizeof(dataSize), 1, dest);
+int toHexFile(void* src, size_t* dataSize, size_t* width, FILE* dest){
+    fwrite(dataSize, sizeof(*dataSize), 1, dest);
     fwrite(&separator, 2, 1, dest);
-    fwrite(&width, sizeof(width), 1, dest);
+    fwrite(width, sizeof(*width), 1, dest);
     fwrite(&separator, 2, 1, dest);
-    fwrite(src, dataSize, 1, dest);
+    fwrite(src, *dataSize, 1, dest);
     fwrite(&separator, 2, 1, dest);
 
     return 0;
